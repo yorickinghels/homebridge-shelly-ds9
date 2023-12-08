378565bf1dfff978554c1e46c5d6c671ac16912d
David S <122049170+cubi1337@users.noreply.github.com>
2023-12-08 23:43:42 +0000

    some tests...

--- src/device-delegates/shelly-plus-pm.ts
+++ src/device-delegates/shelly-plus-pm.ts
@@ -12,11 +12,8 @@ export class ShellyPlusPmDelegate extends DeviceDelegate {
   protected setup() {
     const d = this.device as ShellyPlusPmMini;
 
-   var accessoryPm1 = this.createAccessory("switch", this.device.id, new Pm1Ability(d.pm1));
-   const platformAccessoryPm1 = accessoryPm1.platformAccessory as PlatformAccessory;
-    // unregister the platform accessory
-    this.platform.removeAccessory(platformAccessoryPm1);
-  
+   this.createAccessory("switch", this.device.id, new Pm1Ability(d.pm1));
+
   }
 }
 
